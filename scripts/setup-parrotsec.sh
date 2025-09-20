763b83fd6ed432634388636d6dffb31396b3b1d1
commander-z3r0 <cz3r0.pro@proton.me>
2025-09-20 18:08:22 +0200

    modification parrotsec script

--- scripts/setup-parrotsec.sh
+++ scripts/setup-parrotsec.sh
@@ -36,19 +36,24 @@ install_packages() {
 # ==========================
 
 core() {
+    local script_dir
+    script_dir="$(dirname "$0")"
+
     local core_packages
-    mapfile -t core_packages < config/packages/core.txt
+    mapfile -t core_packages < "$script_dir/config/packages/core.txt"
 
     run "apt update" "Updating package lists"
     install_packages "${core_packages[@]}"
+
     # run "wget -qO- https://deb.parrotsec.org/parrot/misc/parrotsec.gpg | gpg --dearmor -o /etc/apt/trusted.gpg.d/parrot-archive-keyring.gpg" "Adding GPG key"
-    run "wget https://deb.parrot.sh/parrot/pool/main/p/parrot-archive-keyring/parrot-archive-keyring_2024.12_all.deb" "Unpacking parrot-archive-keyring (2024.12) over (2021.7+parrot2)"
+    run "wget https://deb.parrot.sh/parrot/pool/main/p/parrot-archive-keyring/parrot-archive-keyring_2024.12_all.deb" "Unpacking parrot-archive-keyring (2024.12)"
     run "apt install ./parrot-archive-keyring_2024.12_all.deb" "Install new GPG key"
-    run "cp config/system/etc/apt/sources.list /etc/apt/sources.list" "Copying sources.list"
-    run "cp -r config/system/etc/apt/sources.list.d/* /etc/apt/sources.list.d" "Copying sources.list.d"
-    run "cp config/system/etc/apt/listchanges.conf /etc/apt/listchanges.conf" "Copying listchanges.conf"
+
+    run "cp $script_dir/config/system/etc/apt/sources.list /etc/apt/sources.list" "Copying sources.list"
+    run "cp -r $script_dir/config/system/etc/apt/sources.list.d/* /etc/apt/sources.list.d" "Copying sources.list.d"
+    run "cp $script_dir/config/system/etc/apt/listchanges.conf /etc/apt/listchanges.conf" "Copying listchanges.conf"
     run "apt update" "Updating package lists"
-    run "cp config/system/etc/os-release /etc/os-release" "Copying os-release"
+    run "cp $script_dir/config/system/etc/os-release /etc/os-release" "Copying os-release"
     run "apt update" "Updating package lists"
     run "apt upgrade -y" "Upgrading packages"
 
